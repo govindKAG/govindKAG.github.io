def1c041b23c66d08f6b4f7b4480f809a1a80a81
Govind Somalal <govindsomalal7@gmail.com>
2022-03-03 18:40:12 +0530

    updating the dark theme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,17 +3,17 @@ document.body.style.color = sessionStorage.getItem('cc');
 function theme() {
      if ( sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
          
-            sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-            sessionStorage.setItem('cc', '#777');
+            sessionStorage.setItem('bg', 'rgb(30,30,34)');
+            sessionStorage.setItem('cc', '#dddde1');
             
          
      }
     else if (sessionStorage.getItem('bg') == null || undefined) {
-        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-        sessionStorage.setItem('cc', '#777');
+        sessionStorage.setItem('bg', 'rgb(30,30,34)');
+        sessionStorage.setItem('cc', '#dddde1');
         
     }
-    else if( sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
+    else if( sessionStorage.getItem('bg') === 'rgb(30,30,34)') {
         
         sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
         sessionStorage.setItem('cc', '#333');
